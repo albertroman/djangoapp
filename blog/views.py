d1fbd6b289a90302a5e09de8e7a6bcd72cbc42c7
Irvin Tum <irvin_atr@hotmail.com>
2016-09-30 08:58:39 -0600

    cambie el thml para la pagina

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+#from .models import Postear
+#from django.utils import timezone
 
 # Create your views here.
 
+#def Listar_articulos(request):
+#    postear = Postear.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#    return render(request, 'blog/Listar_articulos.html', {'postear':postear})
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Postear
+
 def Listar_articulos(request):
-        return render(request, 'blog/Listar_articulos.html', {})
+    posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/Listar_articulos.html', {'posts': posts})
