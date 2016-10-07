f24b235e3107a7e1fa4d2584afe7fa720ddd9c11
Irvin Tum <irvin_atr@hotmail.com>
2016-10-06 22:06:51 -0600

    aplicar detalle

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postear
+from django.shortcuts import render, get_object_or_404
 
 def Listar_articulos(request):
     posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/Listar_articulos.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/post_detail.html', {'posts': posts})
