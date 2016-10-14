97680ef5a99d84b8563425fc010797d941dd0bf7
Irvin Tum <irvin_atr@hotmail.com>
2016-10-14 09:46:26 -0600

    modificar y editar

--- blog/views.py
+++ blog/views.py
@@ -10,14 +10,41 @@
 
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect
 from .models import Postear
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def Listar_articulos(request):
     posts = Postear.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/Listar_articulos.html', {'posts': posts})
 
-
 def post_detail(request, pk):
     posts = get_object_or_404(Postear, pk=pk)
-    return render(request, 'blog/post_detail.html', {'posts': posts})
+    return render(request, 'blog/post_detail.html', {'post': posts})
+
+def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.fecha_publicacion = timezone.now()
+                post.save()
+                return redirect('blog.views.post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+        post = get_object_or_404(Postear, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.autor = request.user
+                post.save()
+                return redirect('blog.views.post_detail', pk=post.pk)
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
